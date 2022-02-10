cc117f3ce966e4bd65143699d3140de0c4ded93c
SUSHMAGANDAM <99244274+SUSHMAGANDAM@users.noreply.github.com>
2022-02-10 14:23:10 +0530

    Add files via upload

---
+++ string methods part2.py
@@ -0,0 +1,66 @@
+# a='hello','python'
+# b=' world '.join(a)
+# print(b)
+# d="hey"
+# c="*".join(d)
+# print(c)
+# e="hey"
+# f="".join(e)
+# print(f)
+# h="hey",
+# l="*".join(h)
+# print(l)
+# k="hey"," "
+# j="*".join(k)
+# print(j)
+# c="python progrmming"
+# print(c.removeprefix('p'))
+# print(c.removeprefix("P"))
+# print(c.removesuffix("g"))
+# print(c.removesuffix("ing"))
+# print(c.removesuffix("programming"))
+# s="hello python"
+# print(s.isalnum())
+# k="hellopython"
+# print(k.isalnum())
+# d="hellopython33"
+# print(d.isalnum())
+# k="hello python"
+# print(k.isalpha())
+# j=("hellopython")
+# print(j.isalpha())
+# u="hellopythom33"
+# print(u.isalpha())
+# h="print python"
+# print(h.isascii())
+# k=chr(0)
+# j=chr(32)
+# print(k.isspace())
+# print(j.isspace())
+# print(ord("w"))
+# print(ord("r"))
+# print(ord("m"))
+# print(chr(0))
+# print(chr(32))
+# print(chr(112))
+# print(chr(114))
+# print(ord("R"))
+# print(chr(82))
+# s=bin(6)
+# print(s,type(s))
+# print(0b110)
+# print(0B110)
+# s=oct(66)
+# print(s)
+# print(0o102)
+# print(0O102)
+# s=hex(23)
+# print(s,type(s))
+# print(0x17)
+# print(0X17)
+p="pyTHon prograMMinG"
+print(p.swapcase())
+f=4 ; s=6
+print(f,s)
+del f,s
+print(f,s)
